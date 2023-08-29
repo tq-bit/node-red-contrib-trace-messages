bfc6d1cf7e28c37f99d42b5a518ebb931bcbbf35
Tq-bit <t.quante@outlook.com>
2023-08-29 20:24:00 +0200

    move navbar to separate component

--- trace/client/src/components/AppNav.vue
+++ trace/client/src/components/AppNav.vue
@@ -1,14 +1,18 @@
 <script setup>
-const emit = defineEmits(['update:modelValue']);
-const props = {
-  showNav: Boolean
-}
+import { RouterLink, useRouter } from 'vue-router'
+const emit = defineEmits(['update:modelValue', 'clickNavItem']);
+const props = { showNav: Boolean }
+const router = useRouter();
 </script>
 
 <template>
   <v-navigation-drawer v-model="props.showNav">
     <v-list>
-      <v-list-item title="Navigation drawer"></v-list-item>
+      <v-list-item v-for="item in router.getRoutes()" :key="item.path" :value="item">
+        <RouterLink :to="item.path" style="text-decoration: none; color: inherit">
+          <v-list-item-title v-text="item.name"></v-list-item-title>
+        </RouterLink>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
--- trace/client/src/router/router.js
+++ trace/client/src/router/router.js
@@ -3,9 +3,13 @@ import Home from '../components/Home.vue'
 import About from '../components/About.vue'
 
 const routes = [{
-  path: "/", component: Home,
+  path: "/",
+  component: Home,
+  name: "Home"
 }, {
-  path: "/about", component: About
+  path: "/about",
+  component: About,
+  name: "About"
 }]
 
 const router = createRouter({
