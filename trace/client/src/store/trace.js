8b753261b3c865a0aadd8f5965feced48647f5e8
Tq-bit <t.quante@outlook.com>
2023-08-29 18:23:06 +0200

    setup proper store

--- trace/client/src/store/trace.js
+++ trace/client/src/store/trace.js
@@ -1,16 +1,30 @@
 // stores/counter.js
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useTraceStore = defineStore('trace', {
-  state: () =>  ({ traceLists: [], error: null }),
-  actions: {
-    connect(endpoint) {
-      const events = new EventSource(endpoint || "http://localhost:1880/api/sse");
+	state: () => ({
+		traceLists: [
+			{
+				id: 0,
+				receivedAt: new Date().toLocaleString(),
+				list: [],
+			},
+		],
+		error: null,
+	}),
+	actions: {
+		connect(endpoint) {
+			const events = new EventSource(endpoint || 'http://localhost:1880/api/sse');
 
-      events.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        this.traceLists.push(data);
-      }
-    }
-  },
-})
+			events.onmessage = (event) => {
+				const data = JSON.parse(event.data);
+				const entry = {
+					receivedAt: new Date().toLocaleString(),
+					id: this.traceLists.length + 1,
+					list: data.traceList,
+				};
+				this.traceLists.push(entry);
+			};
+		},
+	},
+});
